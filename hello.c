732e0807dd44665b9b9622de4d27a078f14f115c
coderrick <tuc18191@temple.edu>
2017-09-12 20:20:43 -0400

    Eeehh initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/*
+Compiling a C program:
+1) Inovke the C compile on your source code file
+   gcc [-compiler flags][input params][source file .c name]
+
+2) Run progam by using the a.out executable create by the gcc compiler
+
+
+
+
+*/
+
+#include <stdio.h>
+
+main()
+{
+  printf("hello world\n");
+}
